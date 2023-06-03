6428d58c35f7b95451553d0d8727b778b03fd83b
K <thekhoi.k@gmail.com>
2023-06-03 14:09:40 +0700

    edit router temporarily

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,16 +1,12 @@
 import { Route, Routes} from "react-router-dom";
 import Example from "../pages/Example/Example";
 import Home from "../pages/Home";
-import SignIn from "../pages/SignIn";
-import SignUp from "../pages/SignUp";
 
 const Routers = () => {
   return (
     <Routes>
         <Route path="/example" element={<Example />}/>
         <Route path="/" element={<Home />}/>
-        <Route path="/login" element={<SignIn />}/>
-        <Route path="/signup" element={<SignUp />}/>
     </Routes>
   )
 }
