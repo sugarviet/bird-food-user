9ff298d4fef4c3b2dc6de47b9016ed4541eb1c3d
Viet Dang <vietdanghoang1705@gmail.com>
2023-06-08 15:28:18 +0700

    refactor navbar, done cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,10 @@
-import { CloseOutlined } from "@ant-design/icons";
-import { Button, Input, List, Row, Col } from "antd";
+import { Button, Row, Col } from "antd";
 import styles from "./Cart.module.css";
+import { SendOutlined } from "@ant-design/icons";
+
+
 import useCart from "./hooks/useCart";
+import CartItems from "./components/CartItems";
 
 const Cart = () => {
   const { calculateTotalPrice, items, removeFromCart, updateQuantity } =
@@ -17,77 +20,21 @@ const Cart = () => {
         <div className={styles.cartBody}>
           <Row className={styles.cartColumns}>
             <Col span={12}>
-              <p>Item</p>
+              <p className={styles.cartColumnsTitle}>Item</p>
             </Col>
             <Col span={8}>
-              <p style={{ textAlign: "center", marginRight: "10em" }}>
-                Quantity
-              </p>
+              <p className={styles.cartColumnsQuantity}>Quantity</p>
             </Col>
             <Col span={4}>
-              <p style={{ textAlign: "end" }}>Price</p>
+              <p className={styles.cartColumnsPrice}>Price</p>
             </Col>
           </Row>
-          <List
-            dataSource={items}
-            renderItem={(item) => (
-              <List.Item className={styles.cartItem}>
-                <div className={styles.cartItemDetails}>
-                  <div style={{ display: "flex", alignItems: "center" }}>
-                    <Button
-                      type="ghost"
-                      shape="circle"
-                      className={styles.deleteIcon}
-                      onClick={() => removeFromCart(item.id)}
-                    >
-                      <CloseOutlined />
-                    </Button>
-                    <div className={styles.productInfo}>
-                      <div className={styles.imgWrapper}>
-                        <img
-                          src="https://images.thdstatic.com/productImages/3c373a6d-5ae6-45ec-ab29-6bd404bfb1b9/svn/pennington-bird-seed-food-100542054-64_600.jpg"
-                          alt=""
-                        />
-                      </div>
-                      <div
-                        style={{
-                          wordWrap: "break-word",
-                          display: "block",
-                          width: "100%",
-                          height: "100px",
-                          zIndex: "1",
-                        }}
-                      >
-                        <small>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Fuga, minus alias! Suscipit{" "}
-                        </small>
-                      </div>
-                    </div>
-                  </div>
-                </div>
 
-                <div>
-                  <Input
-                    className={styles.quantityInput}
-                    type="number"
-                    value={item.quantity}
-                    onChange={(e) =>
-                      updateQuantity(item.id, parseInt(e.target.value, 10))
-                    }
-                    min={1}
-                    onBlur={(e) => {
-                      if (e.target.value.trim() === "") {
-                        removeFromCart(item.id);
-                      }
-                    }}
-                  />
-                </div>
-                <div className={styles.cartItemControls}>
-                  <p className={styles.itemPrice}>${item.price}</p>
-                </div>
-              </List.Item>
-            )}
+          {/* Render all the items in the cart */}
+          <CartItems
+            items={items}
+            removeFromCart={removeFromCart}
+            updateQuantity={updateQuantity}
           />
         </div>
 
@@ -97,7 +44,7 @@ const Cart = () => {
             <span className={styles.priceValue}>${calculateTotalPrice()}</span>
           </div>
           <Button type="primary" className={styles.checkoutButton}>
-            Proceed to Checkout
+            Proceed to Checkout <SendOutlined />
           </Button>
         </div>
       </div>
